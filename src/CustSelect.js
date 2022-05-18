35689ef16843ca40426d60132ea776536f972f19
meet_acharya <97159442+meet2196@users.noreply.github.com>
2022-05-18 09:22:11 +0530

    Add files via upload

---
+++ src/CustSelect.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Select from 'react-select'
+
+const customStyles = {
+     menu: (provided, state) => ({
+       ...provided,
+       width: state.selectProps.width,
+       borderBottom: '1px dotted pink',
+       color: state.selectProps.menuColor,
+       padding: 10,
+       backgroundColor:'rgba(400,200,60,10)'
+     }),
+   
+     control: (_, { selectProps: { width }}) => ({
+       width: width
+     }),
+   
+     singleValue: (provided, state) => {
+       const opacity = state.isDisabled ? 0.5 : 1;
+       const transition = 'opacity 300ms';
+   
+       return { ...provided, opacity, transition };
+     }
+   }
+
+
+function CustSelect ({style,label,options,onChange, defaultValue}, isMulti){
+    return <div style= {style}>
+         <h1>{label}</h1>
+         <Select styles={customStyles} isMulti={isMulti} options={options} onChange={onChange} defaultValue={defaultValue}/>
+    </div>
+}
+
+export default CustSelect;
